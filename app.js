f297599b5fcfe1580e02da0c9055e298df6cae82
subhamishra <subhadarshi@softsuave.com>
2020-11-06 15:25:11 +0530

    modify style

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/appointments', appointmentRouter);
-app.use('/lookuptype', lookuptype);
+app.use('/lookup', lookuptype);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/lookuptype.js
+++ routes/lookuptype.js
@@ -2,69 +2,36 @@ var express = require('express');
 var router = express.Router();
 const lookuptype = require('../model/lookuptype.js')
 
-
-router.get('/list', async function (req, res, next) {
-    const data = await lookuptype.lookuptype('');
-    const appUser = data.result && data.result.length > 0 ? data.result : {};
-    if (!appUser) {
-        res.send({
-            isError: true,
-            error: data.err
-        });
-    } else {
-       res.send({
-           data: appUser
-       });
-    }
+router.get('/types', async function (req, res, next) {
+  const data = await lookuptype.lookuptype('');
+  const appUser = data.result && data.result.length > 0 ? data.result : {};
+  if (!appUser) {
+    res.send({
+      isError: true,
+      error: data.err
+    });
+  } else {
+    res.send({
+      data: appUser
+    });
+  }
 });
 
-router.get('/list/:id', async function (req, res, next) {
-    const Id = req.params.id;
-    const data = await lookuptype.lookupTypeId(Id,'');
-    const appUser = data.result && data.result.length > 0 ? data.result : {};
-    if (!appUser) {
-        res.send({
-            isError: true,
-            error: data.err
-        });
-    } else {
-        res.send({
-            data: appUser
-        });
-    }
+router.get('/list/:typeId', async function (req, res, next) {
+  const typeId = req.params.typeId;
+  const data = await lookuptype.lookupTypeId(typeId, '');
+  const appUser = data.result && data.result.length > 0 ? data.result : {};
+  if (!appUser) {
+    res.send({
+      isError: true,
+      error: data.err
+    });
+  } else {
+    res.send({
+      isError: false,
+      data: appUser
+    });
+  }
 });
 
 module.exports = router;
-
-
-
-
-//RUF__________________
-
-//
-// router.get('/', lookuptype);
-//
-// function lookuptype(column, value) {
-//     const SQL = `select * from lookuptype`;
-//     return new Promise((resolve, reject) => {
-//         pool.query(SQL, [value], (err, result) => {
-//             if (err) {
-//                 console.log(err);
-//                 resolve({
-//                     isError: true,
-//                     err: err,
-//                 })
-//             } else {
-//                 resolve({
-//                     isError: false,
-//                     result: result
-//                 })
-//             }
-//         });
-//     })
-// }
-//
-// /* GET home page. */
-// router.get('/lookuptype', function(req, res, next) {
-//     res.render('index', { title: 'Express' });
-// });
