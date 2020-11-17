df93da712e946fbb8beb5cebf436d68de2357882
AKSHAY_KUMAR <akshay.kumar@softsuave.com>
2020-11-17 18:13:10 +0530

    lookup data from datafile to database

--- helpers/seed/data/lookup.js
+++ helpers/seed/data/lookup.js
@@ -33,7 +33,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Stroller.png",
-          "Description": "Test",
+          "Description": "Parent's tray with cup holders and storage to carry essentials.\\r\\nChild's tray with cup holder pivots for no-fuss stroller entry.\\r\\nLarge storage basket can hold extra supplies for your child\\r\\nMulti-position, reclining seat, creates a comfortable position for your child.",
         },
         points: "1500",
     },
@@ -42,7 +42,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Exersaucer.png",
-          "Description": "Test"
+          "Description": "Provides baby with secure learn and play environment.\\r\\nGross motor skills, fine motor skills, object exploration, cause/effect learning, self-awareness, tactile development, visual development, object permanence, hand-eye coordination.\\r\\nPhysical exercise to develop baby's neck, back and leg muscles and improve gross motor skills."
         },
         points: "1000",
         
@@ -52,7 +52,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "HighChair.png",
-          "Description": "Test",
+          "Description": "The high chair has three defining features that make it amazing: convertible design, removable feeding tray and style.",
         },
         points: "800",
     },
@@ -61,7 +61,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Carseat.png",
-          "Description": "Test",
+          "Description": "Infant Car Seat fitting newborns and babies from 4-35 pounds includes a stay-in-car convenience base that minimizes overall parent effort.",
         },
         points: "800",
     },
@@ -70,7 +70,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Bouncyseat.png",
-          "Description": "Test",
+          "Description": "Spin up sweet moments with a bouncer.\\r\\nVibrations help baby relax.\\r\\nDelightfully spinnable toys catch a giggle.",
         },
         points: "600",
     },
@@ -79,7 +79,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Swing.png",
-          "Description": "Test",
+          "Description": "Compact baby's swing designed to save space in the home, making it ideal for any room.\\r\\nHeight-adjustable legs allow easy transition between compact and full-size modes, keeping baby at the perfect height.\\r\\nEasy, compact fold to store between uses or when extra space is needed in the home.\\r\\nAdjustable swing speeds to find the right pace to suit baby's mood.",
         },
         points: "600",
     },
@@ -88,7 +88,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "BabyactivityMat.png",
-          "Description": "Test",
+          "Description": "Sit and Play, Tummy Time and Overhead Discovery. \\r\\nLarge peek-a-boo mirror entertains baby.\\r\\nIncludes 4 linkable toys.",
         },
         points: "600",
     },
@@ -97,7 +97,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Babymonitor.png",
-          "Description": "Test",
+          "Description": "Full Viewing Control, a Baby Monitor With Camera Features.\\r\\nFit The Zoom Lens To See Even Closer, Or zoom out with the wide angle lens.",
         },
         points: "400",
     },
@@ -105,7 +105,7 @@ const ItemsData = {
         displayValue: "Diaper Bag",
         customStringValue: {
           "ImagePath": "DiaperBag.png",
-          "Description": "Test",
+          "Description": "The interior of the backpack diaper bag includes a padded laptop pocket, mesh pocket organizers and other organization essentials.\\r\\nMaintain the temperature of your baby bottles or baby food in the insulated side pockets, never lose your keys and wallet in the mommy pocket, and quickly grab a diaper out of the mesh diaper organizer.\\r\\nStroller straps and pversized changing pad.",
         },
         points: "300",
     },
@@ -114,7 +114,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "DigitalThermometer.png",
-          "Description": "Test",
+          "Description": "\\r\\nThe soft flexible thermometer tip is designed with the comfort and safety for your beloved infants, babies & children.\\r\\nEasy to clean, waterproof design allows you maximum sanitation to prevent the spread of illness between your toddlers.",
         },
         points: "300",
     },
@@ -123,7 +123,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Diapers.png",
-          "Description": "Test",
+          "Description": "Soft comfort ever, while pulling wetness and mess away.\\r\\nAir Channels allows air to reach baby’s skin to help keep them dry and comfortable.",
         },
         points: "200",
     },
@@ -132,7 +132,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Bathtub.png",
-          "Description": "Test",
+          "Description": "Bath tub offers ultimate comfort and convenience for you and your baby. ",
         },
         points: "200",
     },
@@ -141,7 +141,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "BabyProofingkit.png",
-          "Description": "Test",
+          "Description": "Wide use cabinet/drawer/cupboards locks.\\r\\nSafe corener guards and door finger.\\r\\nGuards for childproofing.\\r\\nOutlet plug covers and sliding glass door locks.",
         },
         points: "200",
     },
@@ -150,7 +150,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Outfits_onesies.png",
-          "Description": "Test",
+          "Description": "Outfits soft and breathable.\\r\\nDesign: Long sleeve onesies cotton rompe,and long pants",
         },
         points: "100",
     },
@@ -159,7 +159,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Wipes.png",
-          "Description": "Test",
+          "Description": "Gentle cleaning for your baby’s sensitive skin",
         },
         points: "100",
     },
@@ -168,7 +168,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Bottles_brush.png",
-          "Description": "Test",
+          "Description": "Baby bottles are deisgned to most closely mimic breastfeeding to reduce bottle rejection.",
         },
         points: "100",
     },
@@ -177,7 +177,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Babybooks_toys.png",
-          "Description": "Test",
+          "Description": "Take along toy is perfect for travel entertainment.\\r\\nEngages baby at home or away as carrier toy.",
         },
         points: "100",
     },
@@ -186,7 +186,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Memorybook.png",
-          "Description": "Test",
+          "Description": "First year baby memory book",
         },
         points: "100",
     },
@@ -195,7 +195,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Mittens_hats_socks.png",
-          "Description": "Test",
+          "Description": "Protects skin baby from self scratching and cold or hot weather.\\r\\nThe Mittens designed with gentle elastic at wrist to keep them on baby's tiny hands.",
         },
         points: "100",
     },
@@ -204,7 +204,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Babybathproducts.png",
-          "Description": "Test",
+          "Description": "Baby Wash & Shampoo that cleanses your baby's skin without drying, making it gentle enough for everyday use.\\r\\nBaby Calming Comfort Bath.\\r\\nBaby Daily Moisture Lotion.\\r\\nStress Relief Body Wash.",
         },
         points: "100",
     },
@@ -213,7 +213,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Blankets.png",
-          "Description": "Test",
+          "Description": "Warm and light, soft touch",
         },
         points: "50",
     },
@@ -222,7 +222,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Hoodedtowel.png",
-          "Description": "Test",
+          "Description": " Soft hooded towels for kids",
         },
         points: "50",
     },
@@ -231,7 +231,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Washcloths.png",
-          "Description": "Test",
+          "Description": "Newborn Bath Towel",
         },
         points: "50",
     },
@@ -240,7 +240,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Bibs.png",
-          "Description": "Test",
+          "Description": "Bibs to catch food",
         },
         points: "50",
     },
@@ -249,7 +249,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "2packpacifiers.png",
-          "Description": "Test",
+          "Description": "Round shaped teat as also used for most baby bottles, making easy and comfortable transition for your baby between feeding and soothing.",
         },
         points: "50",
     },
@@ -258,7 +258,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Infantspoons.png",
-          "Description": "Test",
+          "Description": "Soft tips are gentle on baby's gums",
         },
         points: "50",
     },
@@ -267,7 +267,7 @@ const ItemsData = {
         customValue: null,
         customStringValue: {
           "ImagePath": "Infantbowls.png",
-          "Description": "Test",
+          "Description": " The bowl allow easy eating and even easier cleanup",
         },
         points: "50",
     }
