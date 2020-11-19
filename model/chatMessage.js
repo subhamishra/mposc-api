4b4b03d1bcdf46ef5e91c391c3b11b22df4ca9cd
Monika <monika@softsuave.com>
2020-11-20 00:13:42 +0530

    Fix: Chat users data retrival

--- model/chatMessage.js
+++ model/chatMessage.js
@@ -67,7 +67,7 @@ async function markMessageRead(chatRoomId, currentUserOnlineId) {
         } else {
           resolve({
             isError: false,
-            message : "status updated successfully"
+            message: "status updated successfully"
           })
         }
       });
@@ -93,35 +93,38 @@ function getRecentConversation(chatRoomIds, details, options, currentUserOnlineI
         var appUserId = '';
         var webUserId = '';
         var usersIds = result[0].userIds.split(',');
-        if(details.requestFrom == 'app'){
+        if (details.requestFrom == 'app') {
           appUserId = details.userId;
-          usersIds.forEach(res=>{
-            if(res != details.userId)
+          usersIds.forEach(res => {
+            if (res != details.userId)
               webUserId = res;
           })
-        }else{
+        } else {
           webUserId = details.userId;
-          usersIds.forEach(res=>{
-            if(res != details.userId)
+          usersIds.forEach(res => {
+            if (res != details.userId)
               appUserId = res;
-        })
+          })
         }
 
         const userSQL = `SELECT fullName FROM appuser where userId = ${appUserId} UNION
                               SELECT fullName FROM user where userId = ${webUserId}`;
-        pool.query(userSQL , (err, userNamesresult)=>{
-          if(err){
+        pool.query(userSQL, (err, userNamesresult) => {
+          if (err) {
             resolve({
               isError: true,
               err: err
             })
-          }else{
-            result.users = [{id : appUserId , name:userNamesresult[0].fullName},
-              {id : webUserId , name:userNamesresult[1].fullName}]
+          } else {
+            var users = {
+              recentConversation: result, user: [{ id: appUserId, name: userNamesresult[0].fullName },
+              { id: webUserId, name: userNamesresult[1].fullName }]
+            }
             resolve({
-                      isError: false,
-                      result: result
-                    })
+              isError: false,
+              result: users
+            })
+
           }
         })
       }
