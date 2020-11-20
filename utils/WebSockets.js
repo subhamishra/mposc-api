0d85ffc61a9040c3f44d1b41bc9cb3b2dcc50ef2
Samba Uday Chander <schander@softsuave.com>
2020-11-20 17:12:09 +0530

    - Socket fixes

--- utils/WebSockets.js
+++ utils/WebSockets.js
@@ -1,25 +1,26 @@
-class WebSockets {
-  users = []
-  constructor() {
-    this.users = this.users ? this.users : []
-  }
+// class WebSockets {
+  // var users = []
+  // constructor() {
+  //   this.users = this.users ? this.users : []
+  // }
 
-  connection = (client) => {
+   function connection(client) {
+      var users = [];
     // event fired when the chat room is disconnected
     client.on('disconnect', () => {
       console.log(this)
-      this.users = this.users.filter((user) => user.socketId !== client.id)
+      users = users.filter((user) => user.socketId !== client.id)
     })
     // add identity of user mapped to the socket id
     client.on('identity', (userId) => {
-      this.users.push({
+      users.push({
         socketId: client.id,
         userId: userId,
       })
     })
     // subscribe person to chat & other user as well
     client.on('subscribe', (room, otherUserId = '') => {
-      this.subscribeOtherUser(room, otherUserId)
+      subscribeOtherUser(room, otherUserId);
       client.join(room)
     })
     // mute a chat room
@@ -28,7 +29,7 @@ class WebSockets {
     })
 
     client.on('callUser', (data) => {
-      let userSocket = this.users.find(
+      let userSocket = users.find(
         (user) => user.userId === data.userToCall
       )
       if (userSocket && userSocket.socketId)
@@ -39,7 +40,7 @@ class WebSockets {
     })
 
     client.on('acceptCall', (data) => {
-      let userSocket = this.users.find((user) => user.userId === data.to)
+      let userSocket = users.find((user) => user.userId === data.to)
       if (userSocket && userSocket.socketId) {
         global.io.to(userSocket.socketId).emit('callAccepted', data.signal)
         console.log("Call Accepted", data.signal)
@@ -47,8 +48,8 @@ class WebSockets {
     })
   }
 
-  subscribeOtherUser = (room, otherUserId) => {
-    const userSockets = this.users.filter((user) => user.userId === otherUserId)
+   function subscribeOtherUser(room, otherUserId) {
+    const userSockets = users.filter((user) => user.userId === otherUserId)
     userSockets.map((userInfo) => {
       const socketConn = global.io.sockets.connected[userInfo.socketId]
       if (socketConn) {
@@ -56,6 +57,9 @@ class WebSockets {
       }
     })
   }
-}
+// }
 
-module.exports = new WebSockets()
+module.exports = {
+  connection : connection,
+    subscribeOtherUser: subscribeOtherUser
+}
